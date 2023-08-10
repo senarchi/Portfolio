d4b58476733b25a828636297b423837f534ce32b
Archi <senarchi4@gmail.com>
2023-08-10 22:20:24 +0530

    Portfolio in the making

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.project-carousel').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      adaptiveHeight: true
+    });
+  });
+  
